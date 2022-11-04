0ff38f42e8ff71b0350c019e34905c1f96a6fc9c
Grashaum <rueebli555@gmail.com>
2022-11-04 16:36:07 +0100

    numtrip

--- docs/numtrip/game.py
+++ docs/numtrip/game.py
@@ -1,19 +1,24 @@
-board = [
-    [2, 4, 1, 8, 8],
-    [4, 2, 8, 2, 1],
-    [4, 4, 8, 4, 2],
-    [2, 8, 1, 4, 1],
-    [2, 4, 4, 4, 4]
-]
+def spielfeld():
+    board = [
+        [2, 4, 1, 8, 8, 7],
+        [4, 2, 8, 2, 1, 6],
+        [4, 4, 8, 4, 2, 2],
+        [2, 8, 1, 4, 1, 3],
+        [2, 4, 4, 4, 10, 4]
+    ]
 
-for zeile in board: #board wurde in den letzten Zeilen definiert
-    for zelle in zeile:#Zellen und zeilen sind teile des Spielfelds
-        print(' -', end='')#diese Zeile soll enden, wenn es keine zellen mehr in der Zeile darunter hat; für jede Zelle einen Strich
+    for zeile in board: #board wurde in den letzten Zeilen definiert
+        
+        for zelle in zeile:#Zellen und zeilen sind teile des Spielfelds
+            print(' -', end='')#diese Zeile soll enden, wenn es keine zellen mehr in der Zeile darunter hat; für jede Zelle einen Strich
+        print(' ')
+        for zelle in zeile:
+            print(f'|{zelle}', end='')# print neue zeile? auf jedenfall kommt vor jede zelle ein |
+        print('|')#Am Schluss von jeder Zeile kommt noch ein |
+
+    for zelle in board[0]:
+        print(' -', end='')# dieser teil ist noch der abschluss; die letzte Zeile wird seperat beschrieben
     print(' ')
-    for zelle in zeile:
-        print(f'|{zelle}', end='')# print neue zeile? auf jedenfall kommt vor jede zelle ein |
-    print('|')#Am Schluss von jeder Zeile kommt noch ein |
 
-for zelle in board[0]:
-    print(' -', end='')# dieser teil ist noch der abschluss; die letzte Zeile wird seperat beschrieben
-print(' ')
+spielfeld()
+...
