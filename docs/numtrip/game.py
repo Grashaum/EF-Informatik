c186f341f6afe08aec9820ee601231c6630d28ec
Grashaum <rueebli555@gmail.com>
2022-11-18 16:33:20 +0100

    zelle & zeile nummeriert

--- docs/numtrip/game.py
+++ docs/numtrip/game.py
@@ -6,11 +6,14 @@ def spielfeld():
         [2, 8, 1, 4, 1, 3],
         [2, 4, 4, 4, 10, 4]
     ]
+    def zellennummer():
+        print('    1  2  3  4  5  6')
+    zellennummer()
     zeilennummer = 1
     for zeile in board: #board wurde in den letzten Zeilen definiert
-        
+        print('  ', end='')
         for zelle in zeile:#Zellen und zeilen sind teile des Spielfelds
-            print(' - - ', end='')#diese Zeile soll enden, wenn es keine zellen mehr in der Zeile darunter hat; für jede Zelle einen Strich
+            print(' --', end='')#diese Zeile soll enden, wenn es keine zellen mehr in der Zeile darunter hat; für jede Zelle einen Strich
         print(' ')
         print(zeilennummer, end=' ')
         zeilennummer = zeilennummer + 1 #zeilen werden nummeriert
@@ -18,11 +21,11 @@ def spielfeld():
             print(f'|{zelle}', end='')# print neue zeile? auf jedenfall kommt vor jede zelle ein |
             if zelle < 10:
                 print(' ', end='')# zahlen werden angepasst; die zehn tanzt nich aus der Reihe
-        print('|')#Am Schluss von jeder Zeile kommt ein |
-
+        print('|')# Am Schluss von jeder Zeile kommt ein |
+    print('  ', end='')
     for zelle in board[0]:
         print(' --', end='')# dieser teil ist noch der abschluss; die letzte Zeile wird seperat beschrieben
-    print(' ')
+    print('')
 
 spielfeld()
 ...
