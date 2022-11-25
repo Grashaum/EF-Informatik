6fa938d2d083f9a25e429fe08cf17fafc82c6efa
Grashaum <rueebli555@gmail.com>
2022-11-25 16:32:39 +0100

    add user interaction

--- docs/numtrip/game.py
+++ docs/numtrip/game.py
@@ -1,13 +1,13 @@
 def spielfeld():
     board = [
-        [2, 4, 1, 8, 8, 7],
-        [4, 2, 8, 2, 1, 6],
+        [2, 4, 1, 8, 8, 8],
+        [4, 2, 8, 2, 1, 4],
         [4, 4, 8, 4, 2, 2],
-        [2, 8, 1, 4, 1, 3],
-        [2, 4, 4, 4, 10, 4]
+        [2, 8, 1, 4, 1, 2],
+        [2, 4, 4, 4, 16, 4]
     ]
     def zellennummer():
-        print('    1  2  3  4  5  6')
+        print('    A  B  C  D  E  F')
     zellennummer()
     zeilennummer = 1
     for zeile in board: #board wurde in den letzten Zeilen definiert
@@ -26,6 +26,33 @@ def spielfeld():
     for zelle in board[0]:
         print(' --', end='')# dieser teil ist noch der abschluss; die letzte Zeile wird seperat beschrieben
     print('')
+def verify_input(raw):
+     if not raw[:3].isnumeric():
+        print('Die Ziffern müssen Zahlen sein!')
+        return False
+def text_input(text):
+    text = text.upper().strip()
+    for h in text:
+        if h not in 'ABCDEF':
+            print('Die spalte sollte ein Buchstabe sein')
+            return False
+
 
 spielfeld()
-...
+zinput = input('Geben Sie die Zeile des Feldes ein (Form: Zahl(1-5)): ')
+tinput = input('Geben Sie die spalte des Feldes ein (Form: Buchst.(a-f)): ')
+while not verify_input(zinput):
+    zinput = input('Geben Sie die Zeile des Feldes ein (Form: Zahl(1-5)): ')
+produkt = verify_input(zinput)
+if produkt:
+    print('Produkt', produkt)
+else:
+    print('Fehlerhafte Eingabe!')
+
+while not text_input(tinput):
+    tinput = input('Geben Sie die spalte des Feldes ein (Form: Buchst.(a-f)): ')
+produkt = text_input(tinput)
+if produkt:
+    print('Produkt', produkt)
+else:
+    print('Fehlerhafte Eingabe!')
