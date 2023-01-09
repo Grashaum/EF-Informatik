61e15d1d8fb6647d5ac46a39e6f4ada16567fa32
Grashaum <rueebli555@gmail.com>
2023-01-09 22:33:55 +0100

    try fixing docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,7 +35,6 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
